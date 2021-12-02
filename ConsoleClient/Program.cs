62182d28c26758c26779af3a3954f5d2f65a9e66
Kudkuli Shyamasundara <shyamasundara.kudkuli@volvo.com>
2021-12-02 16:08:56 +0530

    Add project files.

---
+++ ConsoleClient/Program.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNet.SignalR.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class Program
+    {
+
+        public static async Task Main(string[] args)
+        {
+             Console.WriteLine("Press Ctrl + C to close application");
+            using (var hubConnection = new HubConnection("https://localhost:44335"))
+            {
+                IHubProxy ChatHub = hubConnection.CreateHubProxy("ChatHub");
+                await hubConnection.Start();
+                while (true)
+                {
+                    Console.WriteLine("enter your message");
+                    string message = Console.ReadLine();
+
+                    await ChatHub.Invoke("Send", "titls", "shyam", message);
+                    Console.WriteLine();
+
+                }
+            }
+
+        }
+    }
+}
---
+++ UIClient/MyHub1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+namespace SignalRChat
+{
+    public class ChatHub : Hub
+    {
+        public void Send(string titlsd, string name, string message)
+        {
+            // Call the broadcastMessage method to update clients.
+            Clients.All.broadcastMessage(titlsd, name, message);
+        }
+    }
+}
