ca82ab70aaa5ea28bdb0cfb1d687f8b8790ab9c1
Shyamasundara K <shyam.kz@gmail.com>
2022-01-20 08:09:35 +0530

    database backpane added

--- UIClient/Startup1.cs
+++ UIClient/Startup1.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.Owin;
+﻿using Microsoft.AspNet.SignalR;
+using Microsoft.Owin;
+using Microsoft.AspNet.SignalR.Configuration;
 using Owin;
 [assembly: OwinStartup(typeof(SignalRChat.Startup))]
 namespace SignalRChat
@@ -8,6 +10,8 @@ namespace SignalRChat
         public void Configuration(IAppBuilder app)
         {
             // Any connection or hub wire up and configuration should go here
+            GlobalHost.DependencyResolver.UseSqlServer("Database=VSSDATA_T53;Server=.;Integrated Security=True;");
+            GlobalHost.Configuration.DefaultMessageBufferSize = 5;
             app.MapSignalR();
         }
     }
